28e7df30e621f79927c1b640ac3f13415f603a4e
hilmifauziyyah <hilmifs2113@gmail.com>
2023-04-17 03:41:42 +0700

    Menambahkan API Mahasiswa

---
+++ TP_MOD9_1302210081/TP_MOD9_1302210081/Controllers/mahasiswaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_MOD9_1302210081;
+
+namespace TP_MOD9_1302210125.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaDbController : ControllerBase
+    {
+        public static List<Mahasiswa> ListMhs = new List<Mahasiswa>
+        {
+            new Mahasiswa("Hilmi Fauziyyah Supriatna", "1302210081"),
+            new Mahasiswa("Marsya Rayani Humaira", "1302213003"),
+            new Mahasiswa("Arya Frandika Daulay", "1302213069"),
+            new Mahasiswa("Ibrahim Mulyo Tsabit", "1302210022"),
+            new Mahasiswa("Ridwan Nur Rahman", "1302210076")
+        };
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return ListMhs;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return ListMhs[id];
+        }
+        [HttpPost]
+        public void Post(Mahasiswa value)
+        {
+            ListMhs.Add(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            ListMhs.RemoveAt(id);
+        }
+    }
+}
